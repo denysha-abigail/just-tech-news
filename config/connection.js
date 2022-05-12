d993d6aa56fcb61e9519dc4ba2436a3efb5b695a
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-12 02:34:25 -0400

    set up environment variables

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+// import Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+// do not have to save to a variable; all we need is for it to execute when we use connection.js and all of the data inside the .env file will be made available at process.env.<ENVIRONMENT-VARIABLE-NAME>
+require('dotenv').config();
+
+// create connection to database, pass in MYSQL information for username and password 
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
+
+// all we are doing here is importing the base Sequelize class and using it to create a new connection to the database; the 'new Sequelize()' function accepts the database name, MYSQL username, and MYSQL password as parameters and then we pass in configuration settings before exporting the connection
