37417f227f4af5034b18264c41e00a620248a967
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-18 17:52:27 -0400

    defined model associations

--- models/index.js
+++ models/index.js
@@ -3,4 +3,16 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+// a user can make many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// but a post only belongs to one user
+// we are defining the relationship of the Post model to the User; again, we declare the link to the foreign key, which is designated at user_id in the Post model
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Post };
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(routes);
 // turn on connection to db and server
 // established connection; the .sync() part means that this is sequelize taking the models and connecting them to associated database tables (if it doesn't find a table, it'll create it for you)
 // if force was set to true, it would drop and re-create all of the database tables on startup
+// changing the force value to true would cause the database connection to sync with the model definitions and associations; by forcing the sync method to true, we will make the tables recreate if there are any association changes, similar to the SQL --> DROP TABLE IF EXISTS; allowing the table to be overwritten and re-created
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
