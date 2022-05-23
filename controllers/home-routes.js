f155466c3f645f5df5c556b64a65d33fbd4bcad6
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-23 05:53:37 -0400

    populated the template with sequelize data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,23 +1,48 @@
 // this file will contain all of the user-facing routes, such as the homepage and login page
-
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-// in previous modules, we've used res.send() or res.sendFile() for the response; because we've hooked up a template engine, we can now use res.render() and specify which template we want to use
-// in this case, we want to use the homepage.handlebars template (the .handlebars extension is implied)
-// res.render() can accept a second argument, an object (which includes all of the data we want to pass to our template)
-  res.render('homepage', {
-      // each property on the object id, post_url, title, etc. become available in the template using the handlebars.js {{ }} syntax
-      id: 1,
-      post_url: 'https://handlebarsjs.com/guide/',
-      title: 'Handlebars Docs',
-      created_at: new Date(),
-      vote_count: 10,
-      comments: [{}, {}],
-      user: {
-          username: 'test_user'
-      }
-  });
+    Post.findAll({
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+        .then(dbPostData => {
+            // we need to serialize the entire array because we need the entire array of posts to be in the template; this will loop over and map each sequelize object into a serialized version of itself; saving the results in a new posts array
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            // pass a single post object into the homepage template
+            // in previous modules, we've used res.send() or res.sendFile() for the response; because we've hooked up a template engine, we can now use res.render() and specify which template we want to use
+            // in this case, we want to use the homepage.handlebars template (the .handlebars extension is implied)
+            // res.render() can accept a second argument, an object (which includes all of the data we want to pass to our template)
+            // dbPostData[0] is data that sequelize is returning as a sequelize object; to serialize the object down to only the properties that you need, you can use sequelize's get() method
+            res.render('homepage', {posts});
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
+
+
 
 module.exports = router;
