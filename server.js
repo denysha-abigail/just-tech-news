2bd36803f01303b31c65f0a4ffc5cf11065269bf
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-23 03:07:41 -0400

    created homepage file and added stylesheet

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const express = require('express');
 const routes = require('./routes');
 // importing connection to sequelize
 const sequelize = require('./config/connection');
+// allows stylesheet to be made available to the client
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// express.static() is a built-in Express.js middleware function that can take all of the contents of a folder and serve them as static assets 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
