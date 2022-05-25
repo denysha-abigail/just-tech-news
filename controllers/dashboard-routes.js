acf017f4a9ebb44524061fe1a1cca80c01f8cc93
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-25 03:19:41 -0400

    added data and logic to the dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,9 +3,45 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// because the dashboard should only display posts created by the logged in user, you can add a where object to the findAll() query that uses the id saved on the session; you'll also need to serialize the Sequelize data before sending it to the template
 router.get('/', (req, res) => {
-    // we'll hardcode the loggedIn property as true on this route, because a user won't even be able to get to the dashboard page unless they're logged in
-    res.render('dashboard', { loggedIn: true });
-  });
+  Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // serialize data before passing to template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      // we'll hardcode the loggedIn property as true on this route, because a user won't even be able to get to the dashboard page unless they're logged in
+      res.render('dashboard', { loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
