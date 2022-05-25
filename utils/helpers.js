f2f2073f67094ffc440245538b7d5ec7e3e953b2
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-25 02:18:08 -0400

    wrote and fulfilled test for data formats

---
+++ utils/helpers.js
@@ -0,0 +1,8 @@
+// exports function that makes a test pass
+module.exports = {
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+            date
+        ).getFullYear()}`;
+    }
+}
