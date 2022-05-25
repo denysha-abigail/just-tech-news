ae45a1a9bdedbd9ad6a36378db6f8f869425a7b9
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-25 02:41:51 -0400

    wrote and fulfilled test for url formats

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,6 +1,7 @@
 // helpers allow us to add small bits of logic or data manipulation to the template itself
 const {format_date} = require('../utils/helpers');
-const { format_plural } = require('../utils/helpers');
+const {format_plural} = require('../utils/helpers');
+const {format_url} = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
@@ -15,3 +16,13 @@ test('format_plural() returns a pluralized word', () => {
     expect(word1).toBe('tigers');
     expect(word2).toBe('lion');
 });
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+  
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
+  });
--- utils/helpers.js
+++ utils/helpers.js
@@ -6,13 +6,23 @@ module.exports = {
         ).getFullYear()}`;
     },
     format_plural: (word, amount) => {
-      if (amount !== 1) {
-        return `${word}s`;
-      }
-  
-      return word;
-    }
+        if (amount !== 1) {
+            return `${word}s`;
+        }
+
+        return word;
+    },
+    // not all URLs will end in / routes; sometimes they'll end with a query string, meaning that we'll want to cut things off before the ? character
+    // we'll also want to account for URLs with much more after the domain
+    // we could write a large switch statement, or a few if-else statements, but luckily, replace() returns the modified string
+    format_url: url => {
+        return url
+            .replace('http://', '')
+            .replace('https://', '')
+            .replace('www.', '')
+            .split('/')[0]
+            .split('?')[0];
+    },
 }
 
 
-    
